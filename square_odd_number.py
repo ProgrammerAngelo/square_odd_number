73d89008e80b2caadaf0c70ee35ec7717a98b052
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-10-13 08:02:49 -0400

    Final Result

--- square_odd_number.py
+++ square_odd_number.py
@@ -1,3 +1,5 @@
+integer_list = [13, 7, 35, 19, 43]
 #add a method to compute the square of odd numbers
-#add a method for inputing the list from the user
+squared = [integer ** 2 for integer in integer_list if integer % 2 != 0]
 #add a method for displaying the answer
+print(squared)
